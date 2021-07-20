8f9e30ef2fda01a43e68aa61d58991e2019820ce
Daniel Nduati <dannduati2@gmail.com>
2021-07-21 00:11:34 +0300

    Updated the db creation script

--- db/script.sql
+++ db/script.sql
@@ -8,8 +8,19 @@ CREATE TABLE patients (
 CREATE TABLE readings(
     id INT AUTO_INCREMENT PRIMARY KEY,
 	reading INT NOT NULL,
-	reps INT,
 	date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    patients_id INT NOT NULL,
+    INDEX par_ind (patients_id),
+    FOREIGN KEY (patients_id)
+        REFERENCES patients(id)
+        ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE reps(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    weight INT NOT NULL,
+    reps INT NOT NULL,
+    date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     patients_id INT NOT NULL,
     INDEX par_ind (patients_id),
     FOREIGN KEY (patients_id)
